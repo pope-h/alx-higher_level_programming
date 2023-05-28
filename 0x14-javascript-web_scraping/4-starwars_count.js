a21ad9f930e323520eeb791f41106ce02aa45aa5
Ekarika Nsemeke <hamatthan@gmail.com>
2023-05-28 01:06:48 +0100

    correct task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,10 +10,16 @@ request(apiURL, function (err, response, body) {
   } else if (response.statusCode !== 200) {
     console.log(response.statusCode);
   } else {
-    const movieData = JSON.parse(body);
-    const movieWithCharacter = movieData.results.filter(movie =>
-      movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(movieWithCharacter.length);
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const fIndex in films) {
+      const filmChars = films[fIndex].characters;
+      for (const cIndex in filmChars) {
+        if (filmChars[cIndex].includes(characterId)) {
+          count++;
+	}
+      }
+    }
+    console.log(count);
   }
 });
